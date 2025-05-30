24e12536998e1fa675efbafa6b76cb9a0782e8ca
kbyeo <kbyeo126555@gmail.com>
2025-05-30 13:20:31 +0800

    Fourth commit (v8): hotfix

--- EduGlobeApp/src/pages/LoginPage.jsx
+++ EduGlobeApp/src/pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 import LoginForm from '../components/LogInForm.jsx';
 import './LoginPage.css';
 function LoginPage() {
-
+    //just show the LoginForm component.
     return (<div>
                 <LoginForm/>
             </div>);
